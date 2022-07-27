9d8842a20a336123d2562a9bd94bb46e63133902
Ziyosh <ziyodaakhmedova1998@gmail.com>
2022-07-27 17:29:59 +0300

    My 4.Final Task commit!

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = 10;
+        second = 20;
+        first = first^second;
+        second = first^second;
+        first = first^second;
+        System.out.println("After swap first: " + first + " second = " + second);
+
+//        System.out.println(first);
+//        System.out.println(second);
+
+
     }
 }
