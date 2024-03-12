4fb02032c34b6e4f68e5ae837ef8abc843c5f6d5
Ziyosh <ziyodaakhmedova1998@gmail.com>
2024-03-12 11:54:41 +0500

    module4-final-task

--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,11 +2,8 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
-        firstSide = 1.0;
-        secondSide = 4.0;
-        thirdSide = 2.0;
 
-        if((firstSide > 0) && (secondSide > 0) && (thirdSide > 0)){
+        if(firstSide >= 0 || secondSide >= 0 || thirdSide >= 0){
             if (((firstSide + secondSide) > thirdSide)
                     && ((firstSide + thirdSide) > secondSide)
                     && ((secondSide + thirdSide) > firstSide)) {
